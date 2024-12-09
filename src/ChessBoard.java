e76f4ebdd91dc33739b6dcffb48aacea85b70bcc
BeshoyEhab <beshoyehabhhmm@gmail.com>
2024-12-09 12:30:41 +0200

    Delete unused comment and fix pieces positions

--- src/ChessBoard.java
+++ src/ChessBoard.java
@@ -1,213 +1,3 @@
-//import javax.swing.*;
-//import java.awt.*;
-//import java.awt.event.MouseAdapter;
-//import java.awt.event.MouseEvent;
-//
-//public class ChessBoard extends JFrame {
-//    private static final int BOARD_SIZE = 8; // Standard chessboard is 8x8
-//    private JPanel gridPanel;
-//    private Color color;
-//    private int[] dims;
-//    private Piece[][] boardState = Piece.getInitialSetup();
-//    private Piece selectedPiece;
-//    private int selectedRow=-1, selectedCol=-1;
-//
-//    public ChessBoard(int[] dims, Color color) {
-//        this.color = color;
-//        this.dims = dims;
-//        setTitle("Chess Board");
-//        setSize(this.dims[0], this.dims[1]);
-//        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-//        setResizable(false);
-//
-//        JPanel boardPanel = new JPanel();
-//        boardPanel.setLayout(new BorderLayout());
-//
-//        gridPanel = getJPanel(this.color); // Initialize grid panel
-//
-//        // Add row labels on the left
-//        JPanel rowLabels = new JPanel(new GridLayout(BOARD_SIZE, 1));
-//        for (int i = BOARD_SIZE - 1; i >= 0; i--) {
-//            JLabel label = new JLabel(String.valueOf(i + 1), SwingConstants.CENTER);
-//            label.setFont(new Font("Arial", Font.BOLD, 16));
-//            rowLabels.add(label);
-//        }
-//
-//        // Add column labels on the bottom
-//        JPanel colLabels = new JPanel(new GridLayout(1, BOARD_SIZE));
-//        for (int i = 0; i < BOARD_SIZE; i++) {
-//            JLabel label = new JLabel(String.valueOf((char) ('A' + i)), SwingConstants.CENTER);
-//            label.setFont(new Font("Arial", Font.BOLD, 16));
-//            colLabels.add(label);
-//        }
-//
-//        boardPanel.add(rowLabels, BorderLayout.WEST);
-//        boardPanel.add(gridPanel, BorderLayout.CENTER);
-//        boardPanel.add(colLabels, BorderLayout.SOUTH);
-//
-//        add(boardPanel);
-//    }
-//
-//    private JPanel getJPanel(Color color) {
-//        JPanel gridPanel = new JPanel(new GridLayout(BOARD_SIZE, BOARD_SIZE));
-//        gridPanel.setBorder(BorderFactory.createLineBorder(Color.BLACK));
-//
-//        // Add chessboard squares
-//        for (int i = 0; i < BOARD_SIZE; i++) {
-//            for (int j = 0; j < BOARD_SIZE; j++) {
-//                JPanel square = getJPanel(color, i, j);
-//
-//                gridPanel.add(square);
-//            }
-//        }
-//
-//        return gridPanel;
-//    }
-//
-//    private JPanel getJPanel(Color color, int i, int j) {
-//        JPanel square = new JPanel();
-//        if ((i + j) % 2 == 0) {
-//            square.setBackground(Color.WHITE);
-//        } else {
-//            square.setBackground(color);
-//        }
-//
-//        int finalI = i;
-//        int finalJ = j;
-//        square.addMouseListener(new MouseAdapter() {
-//            @Override
-//            public void mouseClicked(MouseEvent e) {
-//                handleSquareClick(finalI, finalJ);
-//            }
-//        });
-//        return square;
-//    }
-//
-//    private JPanel getJPanel(JLabel icon, int i, int j) {
-//        JPanel square = new JPanel();
-//        if ((i + j) % 2 == 0) {
-//            square.setBackground(Color.WHITE);
-//        } else {
-//            square.setBackground(color);
-//        }
-//        square.add(icon);
-//
-//        int finalI = i;
-//        int finalJ = j;
-//        square.addMouseListener(new MouseAdapter() {
-//            @Override
-//            public void mouseClicked(MouseEvent e) {
-//                handleSquareClick(finalI, finalJ);
-//            }
-//        });
-//        return square;
-//    }
-//
-//    private void addDot(int row, int col) {
-//        // Convert chessboard coordinates to array indices
-//        // Rows are reversed in the layout
-//        int rowIndex = BOARD_SIZE - 1 - row;
-//
-//        Component component = gridPanel.getComponent(col + BOARD_SIZE * rowIndex);
-//
-//        if (component instanceof JPanel square) {
-//            // Debug: Print square background color
-//            System.out.println("Square Background Color: " + square.getBackground());
-//
-//            square.setLayout(new BorderLayout());
-//            Dot dot = new Dot((dims[0]-20)/8, (dims[1]-20)/8, calculateMidColor(Color.WHITE, this.color));
-//
-//            // Add the dot to the center of the square
-//            square.add(dot, BorderLayout.CENTER);
-//
-//            // Revalidate and repaint the grid panel to ensure layout updates correctly
-//            gridPanel.revalidate();
-//            gridPanel.repaint();
-//
-//            System.out.println("Dot added at position (" + row + ", " + col + ")");
-//        }
-//    }
-//
-//    private Color calculateMidColor(Color color1, Color color2) {
-//        int r = (color1.getRed() + color2.getRed()) / 2;
-//        int g = (color1.getGreen() + color2.getGreen()) / 2;
-//        int b = (color1.getBlue() + color2.getBlue()) / 2;
-//        return new Color(r, g, b);
-//    }
-//
-//    private void handleSquareClick(int row, int col) {
-//        if (selectedPiece == null) {
-//            // Select a piece
-//            selectedPiece = boardState[row][col];
-//            if (selectedPiece != null) {
-//                selectedRow = row;
-//                selectedCol = col;
-//                highlightSquare(row, col, Color.YELLOW);
-//            }
-//        } else {
-//            // Attempt to move the selected piece
-//            if (selectedPiece.canMove(selectedRow, selectedCol, row, col, boardState)) {
-//                movePiece(selectedRow, selectedCol, row, col);
-//            }
-//            clearHighlights();
-//            selectedPiece = null;
-//            selectedRow = -1;
-//            selectedCol = -1;
-//        }
-//    }
-//
-//    private void movePiece(int fromRow, int fromCol, int toRow, int toCol) {
-//        int toIndex = toRow * BOARD_SIZE + toCol;
-//        int fromIndex = fromRow * BOARD_SIZE + fromCol;
-//
-//        boardState[toRow][toCol] = boardState[fromRow][fromCol];
-//        boardState[fromRow][fromCol] = null;
-//
-//        if (boardState[toRow][toCol] != null) {
-//            gridPanel.remove(toIndex);
-//            gridPanel.add(getJPanel(new JLabel(boardState[toRow][toCol].getIcon()), toRow, toCol), toIndex);
-//            gridPanel.remove(fromIndex);
-//            gridPanel.add(getJPanel(color, fromRow, fromCol));
-//        }
-//
-//        repaint();
-//        revalidate();
-//    }
-//
-//    private void highlightSquare(int row, int col, Color color) {
-//        gridPanel.getComponent(row * BOARD_SIZE + col).setBackground(color);
-//    }
-//
-//    private void clearHighlights() {
-//        for (int i = 0; i < BOARD_SIZE; i++) {
-//            for (int j = 0; j < BOARD_SIZE; j++) {
-//                gridPanel.getComponent(i * BOARD_SIZE + j).setBackground((i + j) % 2 == 0 ? Color.WHITE : Color.BLUE);
-//            }
-//        }
-//    }
-//}
-//
-//class Dot extends JComponent {
-//    private static int DOT_SIZE = 10; // Size of the dot
-//    private static Color color;
-//
-//    public Dot(int width, int height, Color color) {
-//        Dot.color = color;
-//        setPreferredSize(new Dimension(width, height));
-//        DOT_SIZE = (int)(width*0.4);
-//        System.out.println(DOT_SIZE);
-//        setBackground(color);
-//    }
-//
-//    @Override
-//    protected void paintComponent(Graphics g) {
-//        super.paintComponent(g);
-//        g.setColor(color); // Color of the dot
-//        g.fillOval((getWidth() - DOT_SIZE) / 2, (getHeight() - DOT_SIZE) / 2, DOT_SIZE, DOT_SIZE);
-//    }
-//}
-
-
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.MouseAdapter;
@@ -267,7 +57,7 @@ public class ChessBoard extends JFrame {
 
     private JPanel getSquare(int i, int j) {
         JPanel square = new JPanel(new BorderLayout());
-        square.setBackground((i + j) % 2 == 0 ? this.color : Color.WHITE);
+        square.setBackground((i + j) % 2 == 0 ? Color.WHITE : this.color);
         // Capture variables for listener
 
         // Add piece icon if present
@@ -415,13 +205,6 @@ public class ChessBoard extends JFrame {
             }
         }
     }
-
-    public static void main(String[] args) {
-        EventQueue.invokeLater(() -> {
-            ChessBoard chessBoard = new ChessBoard(new int[]{800, 800}, Color.BLUE);
-            chessBoard.setVisible(true);
-        });
-    }
 }
 
 class Dot extends JComponent {
@@ -470,8 +253,8 @@ abstract class Piece {
         board[0][0] = new Rook("Black", basePath + "BRook.png");
         board[0][1] = new Knight("Black", basePath + "BKnight.png");
         board[0][2] = new Bishop("Black", basePath + "BBishop.png");
-        board[0][3] = new King("Black", basePath + "BKing.png");
-        board[0][4] = new Queen("Black", basePath + "BQueen.png");
+        board[0][3] = new Queen("Black", basePath + "BQueen.png");
+        board[0][4] = new King("Black", basePath + "BKing.png");
         board[0][5] = new Bishop("Black", basePath + "BBishop.png");
         board[0][6] = new Knight("Black", basePath + "BKnight.png");
         board[0][7] = new Rook("Black", basePath + "BRook.png");
@@ -482,8 +265,8 @@ abstract class Piece {
         board[7][0] = new Rook("White", basePath + "WRook.png");
         board[7][1] = new Knight("White", basePath + "WKnight.png");
         board[7][2] = new Bishop("White", basePath + "WBishop.png");
-        board[7][3] = new King("White", basePath + "WKing.png");
-        board[7][4] = new Queen("White", basePath + "WQueen.png");
+        board[7][3] = new Queen("White", basePath + "WQueen.png");
+        board[7][4] = new King("White", basePath + "WKing.png");
         board[7][5] = new Bishop("White", basePath + "WBishop.png");
         board[7][6] = new Knight("White", basePath + "WKnight.png");
         board[7][7] = new Rook("White", basePath + "WRook.png");
